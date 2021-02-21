5f95f03ac4fa6f2d6b0455ece1870e64c9f6f758
rodrigolima <rodrigo.lima131@gmail.com>
2021-02-21 16:31:52 -0300

    Ajuste EstablishmentDetail e EstablishmenteTypeConverter

---
+++ AppShopping/AppShopping/ViewModels/EstablishmentDetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AppShopping.Services;
+using System.Linq;
+
+namespace AppShopping.ViewModels
+{
+    public class EstablishmentDetailViewModel : BaseViewModel
+    {
+        public Establishment Establishment { get; set; }
+
+        public EstablishmentDetailViewModel()
+        {
+            Establishment = new EstablishmentService().GetEstablishments().First();
+        }
+
+      
+        }
+ }
