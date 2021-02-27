48fc510486794528743ebcf4d64922dba2fd1113
rodrigolima <rodrigo.lima131@gmail.com>
2021-02-27 17:53:18 -0300

    Adicionado Metodo TicketScan utilizando componente ZXingScanner

--- AppShopping/AppShopping.Android/MainActivity.cs
+++ AppShopping/AppShopping.Android/MainActivity.cs
@@ -20,7 +20,10 @@ namespace AppShopping.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+           
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- AppShopping/AppShopping/ViewModels/TicketScanViewModel.cs
+++ AppShopping/AppShopping/ViewModels/TicketScanViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
 
 namespace AppShopping.ViewModels
 {
@@ -28,14 +31,39 @@ namespace AppShopping.ViewModels
 
         public TicketScanViewModel()
         {
-            TicketScanCommand = new Command(TicketScan);
+            TicketScanCommand = new MvvmHelpers.Commands.AsyncCommand(TicketScan);
             TicketPaidHistoryCommand = new Command(TicketPaidHistory);
 
         }
 
-        private void TicketScan()
+        private async Task TicketScan() 
         {
+           var scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += async(result) =>
+            {
+                scanPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(async() =>
+                {
+                    await Shell.Current.Navigation.PopAsync();
+                    Message = result.Text;
+                    TicketProccess(result.Text);
+                });
+            };
+            //TODO Implementar Async
+             await Shell.Current.Navigation.PushAsync(scanPage);
+        }
 
+        private void TicketProccess(string ticketNumber)
+        {
+            try
+            {
+                var ticket = new TicketService().GetTicketInfo(ticketNumber);
+
+                //Todo  - Navegar para a página de pagamento do Ticket
+            }catch(Exception e)
+            {
+                Message = e.Message;
+            }
         }
         private void TicketPaidHistory()
         {
